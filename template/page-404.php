2e6419a40afffb9fa94d02c68a3fe343ed2a66b2
GEUNGA <ungaro.geraldine@gmail.com>
2025-02-19 16:09:14 +0100

    page404

--- template/page-404.php
+++ template/page-404.php
@@ -1,21 +1,31 @@
-<?php
-?>
-<head>
+
 <!DOCTYPE html>
 <html lang="fr">
 <title><?=$title ?></title>
 <link type="text/css" rel="stylesheet" href="css/style.css">
 </head>
 <header>
+<?php
+include "inc/banniere.php"
+?>
+
+<?php
+include "inc/menu.php"
+?>
 
 </header>
 <body>
 
-<div class="erreur">
+<body>
+<div class="container">
 
-<h4>ERREUR 404</h4>
 
-<p> Cette page n'existe pas <p>
+    <h2>Accueil</h2>
+    
+        <img class="imagecontenu" src="images/404.jpg" alt="armoiries">
+   
+
+<h6>Euh, la page demandée est introuvable<h6>
 
 </div>
 

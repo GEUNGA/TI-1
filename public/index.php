600af1c825edab47882891cf8410754bab81ff1f
GEUNGA <ungaro.geraldine@gmail.com>
2025-02-19 11:03:11 +0100

    css de menu et contenu

--- public/index.php
+++ public/index.php
@@ -2,36 +2,40 @@
 
 if(isset($_GET["p"])){
     switch($_GET["p"]){
-        case "page d'accueil":
+        case "accueil":
             $title = "page d'accueil";
-            include "../templates/index.php";
+            include "../template/index.php";
+            break;
+        case "geo":
+            $title ="geo";
+            include "../template/geographie.php";
             break;
         case "hist":
             $title ="hist";
-            include "../templates/histoire.php";
+            include "../template/histoire.php";
             break;
         case "cult":
             $title ="cult";
-            include "../templates/culture.php";
+            include "../template/culture.php";
             break;
         case "gal":
             $title ="gal";
-            include "../templates/galerie.php";
+            include "../template/galerie.php";
             break;
         case "form":
             $title ="form";
-            include "../templates/contact.php";
+            include "../template/contact.php";
             break;
         case "link":
             $title ="link";
-            include "../templates/liens.php";
+            include "../template/liens.php";
             break;
         default:
         $title ="autreValeur";
-        include "../templates/erreur404.php";
+        include "../template/erreur404.php";
     }   
    }   else{
-            include "../templates/accueilView.php";
+            include "../template/index.php";
 
     }
 
